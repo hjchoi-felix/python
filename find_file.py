a050874f58021ac814514a637ba1936d792bc0fe
hjchoi <hjchoi@labfelix.com>
2017-01-26 08:17:18 +0900

    a

--- find_file.py
+++ find_file.py
@@ -4,6 +4,7 @@ import os
 import re
 import win32api
 import fnmatch
+import stat
 
 def get_file_size(file):
     file.seek(0, 2)
@@ -21,6 +22,10 @@ def find_file(root_folder, file_name):
                 result = rex.search(f)
                 if result:
                     full_path = os.path.join(root, f)
+                    #ignore torrent downloading...
+                    if "C:\\Users\\hjchoi\\Documents" in full_path:
+                        print('ignore ' + full_path)
+                        continue
                     founds.append(full_path)
                     print(full_path + ", size=" + format(os.path.getsize(full_path) / 1000, ','))
                     #break
@@ -40,24 +45,49 @@ def move_files(file_name, dest_dir):
     for drive in win32api.GetLogicalDriveStrings().split('\000')[:-1]:
         founds_in_all_drives.extend(find_file(drive, file_name))
     for find_file_path in founds_in_all_drives:
+        if dest_dir in find_file_path:
+            print(find_file_path + ' is dest dir file. pass')
+            continue
         dest_path = dest_dir + os.path.basename(find_file_path)
         print('dest : ' + dest_path)
         if os.path.isfile(dest_path):
             print(dest_path + 'is already exists')
-            continue
-        xcopy(find_file_path, dest_dir)
+            #continue
+        else:
+            xcopy(find_file_path, dest_dir)
         if os.path.isfile(dest_path):
             print(dest_path + ' copied and delete source file')
+            os.chmod(find_file_path, stat.S_IWRITE)
             os.remove(find_file_path)
         
 def xcopy(src, dst):
     os.system('xcopy "%s" "%s"' % (src, dst))
     print(src + ' copied')
-    
 
-#find_file_in_all_drives( 'pgd(.*)892' )
+#find_file_in_all_drives( 'snis(.*)784' )
 #find_file_in_all_drives( '농협' )
 #find_file('L:\\새 폴더 (12)', 'ebod')
 #find_file('L:\\', 'ebod(.*)4')
 
-move_files('mikami_yua', 'D:\\__collections\\__collection_mikami_yua\\')
+move_files('ryouka', 'D:\\__collections\\__collection_asakura_ryouka\\')
+
+'''
+import os, winshell
+from win32com.client import Dispatch
+
+def create_shortcut():
+    desktop = winshell.desktop()
+    path = os.path.join(desktop, r"D:\new_link.lnk")
+    target = r"D:\__collections\__collection_aizawa_haruka\HND-156_aizawa_haruka.mkv"
+    w_dir = r"P:path"
+    icon = r"P:path"
+
+    shell = Dispatch("WScript.Shell")
+    shortcut = shell.CreateShortCut(path)
+    shortcut.Targetpath = target
+    shortcut.WorkingDirectory = w_dir
+    shortcut.IconLocation = icon
+    shortcut.save()
+
+create_shortcut()   
+'''
